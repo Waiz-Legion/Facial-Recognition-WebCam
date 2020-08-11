263f09b8b3308a3f69917b0c9d85e02503d892de
Waiz Khan <68347659+Waiz-Legion@users.noreply.github.com>
2020-08-11 19:22:33 +0530

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 from PIL import Image
-import os  # Path for face image database
-path = '/root/PycharmProjects/Face/Dataset'
+import os  
+path = '/path/to/Dataset'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector = cv2.CascadeClassifier("/root/anaconda3/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml");
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 
 
 def getimagesandlabels(path):
@@ -12,7 +12,7 @@ def getimagesandlabels(path):
     facesamples=[]
     ids = []
     for imagePath in imagepaths:
-        PIL_img = Image.open(imagePath).convert('L')  # grayscale
+        PIL_img = Image.open(imagePath).convert('L')  
         img_numpy = np.array(PIL_img, 'uint8')
         id = int(os.path.split(imagePath)[-1].split(".")[1])
         faces = detector.detectMultiScale(img_numpy)
@@ -23,6 +23,6 @@ def getimagesandlabels(path):
 
 print("\n [INFO] Training faces. It will take a few seconds. Wait ...")
 faces, ids = getimagesandlabels(path)
-recognizer.train(faces, np.array(ids))  # Save the model into trainer/trainer.yml
-recognizer.write('/root/PycharmProjects/Face/Trainer/trainer.yml')
+recognizer.train(faces, np.array(ids))  # Save the model into trainer.yml
+recognizer.write('path/to/trainer.yml')
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
